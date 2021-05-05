28d7087a82ce54347843bb06d35ebf8ad60464d2
unknown <ju25465264@qq.com>
2021-05-05 17:40:59 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-row>
+      <el-col :span="6">
+          TITLE
+      </el-col>
+      <el-col :span="6" :offset="12">
+          LOGIN
+      </el-col>
+  </el-row>
+</template>
+<script>
+  export default {
+    name: 'Header',
+      components: {
+        
+      },
+    data() {
+      return {
+        
+      }
+    },
+    methods:{
+      
+    },
+    computed:{
+      
+    },
+    watch: {
+      
+    }
+  }
+</script>
+<style>
+  
+</style>
